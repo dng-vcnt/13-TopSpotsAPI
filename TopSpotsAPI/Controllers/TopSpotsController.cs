ea1d25d098492ff05ab1a1be701727090b04bf0b
Vincent Duong <vincentduong@Vincents-MacBook-Pro.local>
2016-08-24 17:18:59 -0700

    Initial commit

---
+++ TopSpotsAPI/Controllers/TopSpotsController.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Http;
+using TopSpotsAPI.Models;
+
+namespace TopSpotsAPI.Controllers
+{
+    public class TopSpotsController : ApiController
+    {
+        // GET: TopSpots
+        public IEnumerable<TopSpots> Get()
+        {
+            // Read file into string and deserialize JSON to a type
+            TopSpots[] topSpot = JsonConvert.DeserializeObject<TopSpots[]>(File.ReadAllText(@"z:\dev\05-SanDiegoTopSpots-Angular\topspots.json"));
+
+            return topSpot;
+
+        }
+
+    }
+}
